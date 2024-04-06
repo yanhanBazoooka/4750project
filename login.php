142bafbf4e55204e22f2303575a89bd3a5495c6c
Tz <112019158+tr2bx@users.noreply.github.com>
2024-04-06 16:41:55 -0400

    no more secret

--- login.php
+++ login.php
@@ -3,7 +3,7 @@
 <head>
     <title>Google Sign-In Demo</title>
     <script src="https://apis.google.com/js/platform.js" async defer></script>
-    <meta name="google-signin-client_id" content="890124439981-efpmk1iiv3b3h4abvfits56h1r3lukjm.apps.googleusercontent.com">
+    <meta name="google-signin-client_id" content="xxx">
 </head>
 <body>
 <div class="g-signin2" data-onsuccess="onSignIn"></div>
